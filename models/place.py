b4054e309ce0397a923670cd09304f32345351ad
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-09-19 18:39:15 +0300

    Place

--- models/place.py
+++ models/place.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
