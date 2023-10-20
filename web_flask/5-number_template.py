ef255c584e2ef60ff223f8c74c008919cc9daeb0
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-10-20 21:23:54 +0300

    fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,7 +37,7 @@ def print_number(n):
     return f"{n} is a number"
 
 
-@app.route('/number_template/<int:n>')
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     return render_template('5-number.html', number=n)
 
